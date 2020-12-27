2ba0d42fca8b98b418b7d9a5890019132da1f240
Nikita Shumskii <lordsxrom@gmail.com>
2020-12-27 22:21:09 +0300

    initial commit

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ImageSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.codinginflow.imagesearchapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ImageSearchApplication : Application() {
+
+
+
+}
